c7a3cfae18e6123e55575e2d2929f0602ba0047d
HARISHJAYARAMJ <40231207+HARISHJAYARAMJ@users.noreply.github.com>
2018-06-13 17:17:27 +0530

    Create s69.c

---
+++ CODEKATA/s69.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int a[100],i,j,val,n,max;
+	 scanf("%d\n",&n);
+	 for(i=0;i<=n;i++)
+           {
+	scanf("%d\t",&a[i]);
+	
+            }
+       max=a[0];
+       for(i=0;i<=n;i++)
+         {
+            if(a[i]>max)
+          {
+       	max=a[i];
+           }
+         }
+    printf("%d",max);
+    return 0;
+	
+}
+    
+© 2018 GitHub, Inc.
