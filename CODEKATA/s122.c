cfe86475e149e5210371137ff226e46082ad82fe
HARISHJAYARAMJ <40231207+HARISHJAYARAMJ@users.noreply.github.com>
2018-06-13 14:48:42 +0530

    Create s122.c

---
+++ CODEKATA/s122.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+	 int b;
+	 scanf("%d\n",&b);
+	 if(b%2==0)
+	 {
+	 	printf("Even number");
+	 }
+	 else
+	 {
+	    printf("Odd number");	
+	 }
+	return 0;
+}
